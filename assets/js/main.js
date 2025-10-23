e8431729e6697a8c0c29882f94c7b8dabff62c06
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-10-23 16:23:26 +0200

    completed bonus of exercise

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,32 +17,45 @@
 
 const url = "https://flynn.boolean.careers/exercises/api/random/mail"
 
-const emailListEl = document.getElementById("email-list")
+const emailListEl = document.getElementById("mail-list")
+
+const generateBtnEl = document.getElementById("generate-btn")
 
 // Avvio un ciclo per 10 volte
 
-for (i = 0; i < 10; i++) {
+function appendRandomMail() {
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            // -estraggo il dato con la mail
-            let eMail = data["response"]
-            console.log(eMail);
-            //creo un li
-            let li = document.createElement("li")
-            // inserisco il testo nel li
-            li.innerText = eMail
-            console.log(li);
+    for (i = 0; i < 10; i++) {
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                // -estraggo il dato con la mail
+                let eMail = data["response"]
+                console.log(eMail);
+                //creo un li
+                let li = document.createElement("li")
+                li.className = 'mb-5'
+                // inserisco il testo nel li
+                li.innerText = eMail
+                console.log(li);
+                // lo aggiungo dentro l'ul con appendchild
+                emailListEl.appendChild(li)
+            })
+            .catch(error => {
+                console.error(error);
+            })
 
 
-            // lo aggiungo dentro l'ul con appendchild
-        }
-        )
-
+    }
 }
 
+appendRandomMail()
 
+function resetandrepeat(){
+    emailListEl.innerHTML = ""
+    appendRandomMail()
+}
 
-
+generateBtnEl.addEventListener("click",resetandrepeat)
