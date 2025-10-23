ee6eb64454e9c6140e74632b17cdd6bf387e89dc
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-10-23 15:23:07 +0200

    email logged into the console

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,9 +10,28 @@
 // Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
 
 
+
+
+
 // Salvo in variabili gli elementi del DOM con cui devo interagire
 
+const url = "https://flynn.boolean.careers/exercises/api/random/mail"
+
+const emailListEl = document.getElementById("email-list")
+
 // Avvio un ciclo per 10 volte
+
+for (i = 0; i < 10; i++) {
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            let eMail = data["response"]
+            console.log(eMail);
+        })
+
+}
 
 // -estraggo il dato con la mail
 
