edc14e908fc5b4ed7dd510726fd9024bc3bee4f5
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-10-23 15:03:40 +0200

    Create macroareas and add the structure in html

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+// Descrizione
+
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+// Bonus
+
+// Abbellire con CSS o Bootstrap
+// Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
+
+
+// Salvo in variabili gli elementi del DOM con cui devo interagire
+
+// Avvio un ciclo per 10 volte
+
+// -estraggo il dato con la mail
+
+//creo un li
+
+// lo aggiungo dentro l'ul con appendchild
+
